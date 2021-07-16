fbdccf97220896fc730e17d0a2346aeeb1fc8ee8
ANNER RONALDO ESCOBAR CRUZ <aescobarc13@miumg.edu.gt>
2021-07-16 12:40:43 -0600

    SUBIENDO TAREA V1

---
+++ src/main/java/Convertidor.java
@@ -0,0 +1,49 @@
+    
+    import java.util.Scanner;
+
+
+public class Convertidor {
+
+    public static void main(String[] args) {
+  
+        Scanner sc = new Scanner(System.in);
+        
+        String Nombre = "";
+        double Cantidad = 0;
+        int Opcion = 0;
+        
+        System.out.println("HELLO, ENTER YOUR FULL NAME");
+        Nombre = sc.nextLine();
+        System.out.println("ENTER AN AMOUNT");
+        Cantidad = sc.nextInt();
+        System.out.println("");
+        
+        
+        System.out.println("1. Dolares");
+        System.out.println("1. Euros");
+        System.out.println("1. Yuanes");
+        Opcion = sc.nextInt();
+        System.out.println("");
+        
+        switch(Opcion){
+            case 1 -> {
+                double dolares = Cantidad * 7.65;
+                System.out.println("HI "+Nombre+" YOUR AMOUNT IN DOLLARS IS: "+ dolares);
+            }
+                
+            
+            case 2 -> { 
+                double euros = Cantidad * 9.15;
+                System.out.println("HI "+Nombre+" YOUR AMOUNT IN EUROS IS: "+ euros);
+            }
+                
+            case 3 -> { 
+                double yuanes = Cantidad * 1.20;
+                System.out.println("HI "+Nombre+" YOUR AMOUNT IN YUANES IS: "+ yuanes);
+            }
+        }
+       
+            
+    }
+    
+}
